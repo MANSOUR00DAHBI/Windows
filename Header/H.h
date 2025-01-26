a5adbeeb934a70c1be987239b37d00fa459be4ba
MANSOUR00DAHBI <code00net@gmail.com>
2025-01-27 00:58:45 +0100

    initialProject 0.0.2

---
+++ Header/H.h
@@ -0,0 +1,15 @@
+#ifndef H_H 
+#define H_H 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+#include <signal.h>
+#include <assert.h>
+
+#endif // H_H
