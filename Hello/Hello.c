5b82a42542f61b9d601d0cb16705721109b415a3
MANSOUR00DAHBI <code00net@gmail.com>
2025-01-27 00:55:56 +0100

    initialProject 0.0.2

--- Hello/Hello.c
+++ Hello/Hello.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#ifndef HELLO_H
+#define HELLO_H
+//#include"../Header/H.h"
+#define H_H
 
-int main() {
+int Hello(void);
+int c; // character 
+int nc; // Counter Character  
+#endif //Hello 
+
+#ifdef HELLO_H
+
+int Hello(void) {
 	printf("Hello I'm Visual Studio 2022 \n");
+	c = getchar();
+	for (nc = 0;(c = getchar()) != '^';++nc);
+	printf("Number Char : %3d \n",c);
 	return 0;
 }
+
+#endif // HELLO_H
